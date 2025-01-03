21e93788e2a5f763b958f93e607883a9a667e456
Piero Sierra <piero.sierra@outlook.com>
2025-01-03 08:33:18 +0000

    Initial Commit

---
+++ FillUpButtonStyle/FillUpButtonStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FillUpButtonStyleApp.swift
+//  FillUpButtonStyle
+//
+//  Created by Piero Sierra on 03/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FillUpButtonStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
